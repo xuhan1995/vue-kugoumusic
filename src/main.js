108e882317416e46bcecb6237f322f2f3350c999
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-18 23:18:49 +0800

    引入依赖

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import mint from 'mint-ui'
+import axios from 'axios'
+import 'mint-ui/lib/style.css'
+import './assets/css/neat-min.css'
+import './assets/css/style.css'
+Vue.use(mint)
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
