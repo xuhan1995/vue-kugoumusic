c33dbc14b663e42470b889f8d3abe8a51fa7a159
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-20 00:30:21 +0800

    排行榜页头部有回退按钮和title
    回退用到 this.$router.go()

---
+++ src/components/header/rankHead.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="rank-head container" :style="cStyle" id="transparent-header">
+    <a class="rank-head-back" @click="routerBack"></a>  <!-- 回退左三角 -->
+    {{title}}
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props:['cStyle','title'],
+    methods:{
+      routerBack(){
+        this.$router.go(-1);
+      }
+    }
+  }
+</script>
