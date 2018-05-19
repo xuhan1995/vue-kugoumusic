6df120cd22ac703619e5014b2dd7b497ccdeb1cb
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-20 00:35:24 +0800

    头部组件
    头部组件包括三个子组件
    1.logo和搜索的放大镜
    2.切换条navBar
    3.排行榜的回退按钮和title
    要在父组件中用mapGetters从store中引入所有所需的数据

---
+++ src/components/header/head.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="header">
+    <head-logo />
+    <rank-head v-if="head.toggle" :title="head.title" :cStyle="head.cStyle" />
+    <head-nav v-else/>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import headLogo from './headLogo';
+  import headNav from './headNav';
+  import rankHead from './rankHead';
+  import { mapGetters } from 'vuex';
+  export default {
+    computed:{
+      ...mapGetters(['head'])  //用这种方式获取store的getters中的数据,与下面等价
+      // head(){
+      //   return this.$store.getters.head;
+      // }
+    },
+    components:{
+      'head-logo':headLogo,
+      'head-nav': headNav,
+      'rank-head': rankHead,
+    }
+  }
+</script>
