070464db82e56e6df202f64c51e4da7885b5676e
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-20 00:40:16 +0800

    顶部的logo和搜索放大镜图标
    style中，头部用fixed
    要用div包裹img，把div和img的height都设置为100%可使其填满，当然最外层还要有一个div，在这个div里设置希望的高度

---
+++ src/components/header/headLogo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="logo-container">
+    <div class="head-logo">
+      <a href="#">
+        <img src="http://m.kugou.com/v3/static/images/index/logo.png">
+      </a>
+    </div>
+    <div class="head-search" @click="searchRouter">
+      <img src="http://m.kugou.com/v3/static/images/index/search.png">
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    methods:{
+      searchRouter(){
+        this.$store.commit('showDetailPlayer',false);  //关闭播放面板
+        this.$router.push({ path : '/search'});
+      }
+    }
+  }
+</script>
+
+<style type="text/css">
+  .logo-container{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 51px;
+    padding: 10px;
+    z-index: 1050;
+    background-color: #2CA2F9;
+    overflow: hidden;
+  }
+  .head-logo{
+    float: left;
+    height: 100%;  /*这个并把img的height也改成100%可以让图像自适应*/
+  }
+  .head-logo img{
+    height: 100%;
+  }
+  .head-search{
+    float: right;
+    height: 100%;
+  }
+  .head-search img{
+    height: 100%;
+    padding: 3px;
+  }
+</style>
