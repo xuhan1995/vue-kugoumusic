924fa0dd6a9034385bd8ac4b3ed66ca913675283
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-20 00:44:31 +0800

    header中的navBar
    this.$store.commit('setHeadNav',id);
    不要用commit将数据变化反馈给store，commit的第一个参数是store的mutations中的函数名，之后是要传入的参数

---
+++ src/components/header/headNav.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="header-nav">
+    <mt-navbar v-model="headNav" class="container">
+      <mt-tab-item id="head-nav1">新歌</mt-tab-item>
+      <mt-tab-item id="head-nav2">排行</mt-tab-item>
+      <mt-tab-item id="head-nav3">歌单</mt-tab-item>
+      <mt-tab-item id="head-nav4">歌手</mt-tab-item>
+    </mt-navbar>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    computed:{
+      headNav:{
+        get() {
+          return this.$store.getters.headNav
+        },
+        set(id) {
+          const index = id.substr(-1);
+          this.$store.commit('setHeadNav',id);
+          this.goRouter(Number(index));
+        }
+      },
+    },
+    methods:{
+      goRouter (index) {
+        switch (index) {
+          case 1:
+            this.$router.push({path: '/'});
+            break;
+          case 2:
+            this.$router.push({path: '/rank'});
+            break;
+          case 3:
+            this.$router.push({path: '/plist'});
+            break;
+          case 4:
+            this.$router.push({path: '/singer'});
+            break;
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  .header-nav{
+    position: fixed;
+    top: 51px;
+    left: 0;
+    width: 100%;
+    z-index: 1000;
+    border-bottom: 1px solid #e4e4e4;
+  }
+  .container{
+    width: 100%;
+    padding: 0 10px;
+    overflow: hidden;
+  }
+  a{text-decoration: none !important;}
+  .mint-navbar .mint-tab-item.is-selected{margin-bottom: 0}
+  .mint-tab-item {
+    padding: 12px 0 !important;
+  }
+
+  .mint-tab-item-label {
+    font-size: 16px !important;
+  }
+
+</style>
