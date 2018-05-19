4ac8f04393a9a5fcd441ee2afd8e5cd93a554a78
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-19 21:38:09 +0800

    在App.vue中添加头部

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
   <div id="app">
-    <router-view/>
+    <Head />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
-</script>
+  import Head from './components/header/head'
+  export default {
+    name: 'App',
+    components:{
+      Head
+    }
+  }
+  </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
