cb5388229407b091402f374fd8dbfa60f42e522f
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-22 21:42:56 +0800

    底部player-panel组件

---
+++ src/components/playerPanel.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="audio-view" :class="{'audio_panel_hide' : toggleHide}">
+    <!-- audio_panel_hide类是把playerPanel隐藏 -->
+    <audio :src="audio.songUrl" autoplay id="audioPlay" @timeupdate="change" @ended="next"></audio>    <!-- 目前不明timeupdate有什么用 -->
+    <div class="audio-panel-control" @click="togglePanel" :class="{'toggleContral' : toggleHide}">    <!-- toggleContral是圆圈里的上拉和划下 -->
+      <mt-spinner type="fading-circle" :size="27" v-show="audioLoadding"></mt-spinner>0
+    </div>
+    <div class="audio-panel">
+      <img class="player-img" :src="audio.imgUrl" @click="showDetailPlayer">
+      <div class="player-status" @click="showDetailPlayer">
+        <p class="player-title ellipsis">{{audio.title}}</p>
+        <p class="player-singer ellipsis">{{audio.singer}}</p>
+      </div>
+      <div class="player-controls">
+        <span class="player-Play" @click="toggleStatus" :class="{'player-Pause':isPlay}"></span>  <!-- 正在播放应该显示暂停图标 -->
+        <span class="player-nextSong" @click="next"></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  import { mapGetters } from 'vuex'
+
+  export default {
+    data: () => ({
+      toggleHide: false,  //面板隐藏和显示一个上三角一个下三角的flag
+    }),
+    computed:{
+      ...mapGetters(['audio','audioLoadding','showPlayer','isPlay'])
+    },
+    methods:{
+      togglePanel(){
+        this.toggleHide = !this.toggleHide;
+      },
+      change(){
+        let time = jq('#audioPlay')[0].currentTime;  //当前播放时间
+        if (this.currentFlag) {   //目前不明白currentFlag是什么
+          jq('#audioPlay')[0].currentTime = this.audio.currentLength;
+          this.$store.commit('setCurret',false);
+        }
+        else{
+          this.$store.commit('setAudioTime',time);
+        }
+      },
+      next(){
+        this.$store.dispatch('next');
+      },
+      showDetailPlayer(){
+       if (this.showPlayer) {
+         this.$store.commit('showDetailPlayer',true)
+       }
+      },
+      toggleStatus(){
+        if (this.isPlay) {
+          jq('#audioPlay')[0].pause();
+        }
+        else{
+          jq('#audioPlay')[0].play();
+        }
+        this.$store.commit('isPlay',!this.isPlay);
+      },
+    }
+  }
+</script>
+
+<style type="text/css">
+.audio-view{
+  position: fixed;bottom: 0;left: 0;width: 100%;transition: all 0.5s;
+}
+.audio-view .mint-spinner-snake{background-color: #000;margin:4px}
+/*未隐藏下三角*/
+.audio-panel-control{
+  width: 35px;height: 35px;border-radius: 50%;background: rgba(0,0,0,.8) url("../assets/images/close_icon.png") no-repeat center;background-size: 25px;margin-left: 10px;margin-bottom: 10px;overflow: hidden
+}
+/*隐藏后，正三角*/
+.toggleContral{background: rgba(0,0,0,.8) url("../assets/images/open_icon.png") no-repeat center;background-size: 25px !important;}
+.audio-panel{
+  z-index: 999;width: 100%;height: 64px;background-color: rgba(0,0,0,0.8);padding: 7px;overflow: hidden;
+}
+.player-img{
+  height: 100%;float: left;border-radius: 2px;
+}
+.player-status{
+  float:left;width: 50%;height: 100%;margin-left: 3%;padding:5px;color: #fff;
+}
+.player-title{width: 100%;font-size: 16px;overflow: hidden;display: block;}
+.player-singer{display: block;height: 50%;width: 100%;color: #c4c4c4;padding-top: 2px}
+.player-controls{
+  width: 30%;height: 100%;float: right;
+}
+.player-controls span{
+  display: block;width: 50%;height: 100%;float: left;
+}
+.player-Play{
+  background: url("../assets/images/play_icon.png") no-repeat center;background-size:auto 70%;
+}
+.player-Pause{background: url("../assets/images/pause_icon.png") no-repeat center;background-size:auto 70%;}
+.player-nextSong{background: url("../assets/images/next_icon.png") no-repeat center;background-size:auto 70%;}
+/*隐藏面板,此类是加在audio-view即最外层div上的，而audio-view的position是fixed所以改变他的bottom*/
+.audio_panel_hide{
+  bottom:-64px;
+}
+
+.mint-navbar .mint-tab-item.is-selected{margin-bottom: 0}
+</style>
