1ac418b3e74c3ed4c7d9e84aec9b21a206888a1d
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-22 21:22:32 +0800

    mixins内容，播放选中歌曲

---
+++ src/mixins/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+  'PLAY_AUDIO': {
+    methods: {
+      playAudio(index) {
+        let hash = this.songList[index].hash;
+        let info = {
+          songList: this.songList,
+          songIndex: index
+        }
+        this.$store.commit('setListInfo', info);  //不涉及异步用commit
+        this.$store.dispatch('getSong', hash);    //涉及异步用dispatch0,用选中歌曲的hash值得到他的信息
+        this.$store.commit('isPlay', true);    //开始播放啦！
+      }
+    }
+  }
+}
