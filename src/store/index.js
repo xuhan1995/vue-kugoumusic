d92165794cc90e756b265c0169e78468c2953308
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-22 21:44:11 +0800

    在store中添加全局state和函数
    最重要的是actions中的getSongs和next

--- src/store/index.js
+++ src/store/index.js
@@ -11,12 +11,33 @@ const store = new Vuex.Store({
       title:'',
       style: {'background': 'rgba(43,162,251,0)'},
     },
-    showDetailPlayer:false,   //显示歌词面板
+    audio:{  //audio只是选中的歌曲信息，不是歌单信息
+      songUrl: '',  //歌曲url，在audio中播放用这个
+      imgUrl: 'http://m.kugou.com/v3/static/images/index/logo_kugou.png',
+      title: '',
+      singer: '',
+      currentLength: 0,    //已播放的时长
+      songLength: 0,      //歌曲长度
+      currentFlag: false   //
+    },
+    showDetailPlayer:false,   //显示歌词面板,这里叫详细播放器
     headNav: 'head-nav1',  //一开始在第一个nav
+    songListInfo:{  //储存了newSongs页歌单信息和选中歌的index
+      songList: [],
+      songIndex: -1
+    },
+    audioLoadding:false, //歌曲加载中
+    listenCount:0, //听了几首歌
+    showPlayer:false,  //是否能允许detailPlayer，只有首次进入不允许，因为没放歌，detailPlayer，啥都没有
+    isPlay:false,  //是否正在播放
   },
   getters:{
     headNav: state => state.headNav,
     head: state => state.head,
+    audio: state => state.audio,
+    audioLoadding: state => state.audioLoadding,
+    showPlayer: state => state.showPlayer,
+    isPlay: state => state.isPlay,
   },
   mutations:{
     showDetailPlayer: (state,flag) => {
@@ -25,9 +46,64 @@ const store = new Vuex.Store({
     setHeadNav: (state,currentNav) => {
       state.headNav = currentNav;
     },
+    //newSongs页
+    setListInfo: (state,info) => {
+      state.songListInfo = {...(state.songListInfo),...info};
+    },
+    toggleAudioLoadding:(state,flag) => {
+      state.audioLoadding = flag;
+    },
+    setAudio: (state,audio) => {  //把选中的歌曲并入state的audio
+      if (!state.listenCount) {
+        state.showPlayer = true  //首次进入应用时不可打开播放详情
+      }
+      state.listenCount++
+      state.audio = {...(state.audio), ...audio}  //相当于object.assign
+    },
+    setLrc: (state,lrc) => {
+      state.audio = {...(state.audio),lrc}
+    },
+    //player页
+    setCurret: (state,flag) => {
+      state.audio.currentFlag = flag;
+    },
+    setAudioTime: (state,time) => {
+      state.audio.currentLength = time;
+    },
+    isPlay: (state,flag) => {
+      state.isPlay = flag;
+    }
   },
   actions:{
-
+    getSong({commit, state}, hash){
+      commit('toggleAudioLoadding',true);  //让下面Player的图标显示加载
+      axios.get(`/bproxy/yy/index.php?r=play/getdata&hash=${hash}`).then(({data}) => {
+        data = data.data;
+        console.log(data);
+        const songUrl = data.play_url
+        const imgUrl = data.img
+        const title = data.audio_name
+        const songLength = data.timelength / 1000
+        const singer = data.author_name
+        const currentLength = 0
+        const lrc = data.lyrics
+        const audio = {songUrl, imgUrl, title, singer, songLength, currentLength};
+        commit('setAudio',audio);
+        commit('setLrc',lrc);   //lrc是歌词
+        commit('toggleAudioLoadding',false);  //让下面Player的图标加载完成
+      })
+    },
+    next({dispatch,state}){
+      let songList = state.songListInfo.songList;
+      if (state.songListInfo.songIndex == songList.length - 1) {
+        state.songListInfo.songIndex = 0;
+      }
+      else{
+        state.songListInfo.songIndex++;
+      }
+      let hash = songList[state.songListInfo.songIndex].hash;
+      dispatch('getSong',hash);   //传递选中歌曲信息用hash值
+    }
   },
 
 })
