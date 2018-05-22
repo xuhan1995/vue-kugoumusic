e64d4879fa1b9c4f53780e1220895924d1b69f50
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-22 16:21:44 +0800

    在App.vue中添加路由出口和底部player-panel

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
   <div id="app">
     <Head />
+    <div class="main">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+    <player-panel />
   </div>
 </template>
 
 <script>
   import Head from './components/header/head'
+  import playerPanel from './components/playerPanel'
   export default {
     name: 'App',
     components:{
-      Head
+      Head,
+      'player-panel': playerPanel,
     }
   }
   </script>
