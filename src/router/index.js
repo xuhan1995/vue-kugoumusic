fc48359960eeada427a2577c68aa6ad0f6c8ba0b
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-26 16:03:17 +0800

    添加search路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import newSongs from '../views/newSongs'
+import search from '../views/search'
 
 Vue.use(Router)
 
@@ -9,6 +10,10 @@ export default new Router({
     path: '/newSongs',
     component: newSongs,
     alias: '/'
+  },
+  {
+    path: '/search',
+    component: search,
   },
   ]
 })
