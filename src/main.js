20b0c158a05c02ab729ef5f78969a96a49f7da00
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-05-30 16:21:40 +0800

    添加音量icon的CSS

--- src/main.js
+++ src/main.js
@@ -8,10 +8,12 @@ import mint from 'mint-ui'
 import axios from 'axios'
 import 'mint-ui/lib/style.css'
 import './assets/css/neat-min.css'
-import './assets/css/style.css'
 import jq from 'jquery'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './assets/icon/iconfont.css'
+import './assets/css/style.css'
+
 
 Vue.use(ElementUI);
 Vue.use(mint)
