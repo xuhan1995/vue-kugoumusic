605f7deeccd24f632f71a2fde993cfe369b0e295
xuhan1995 <32316162+xuhan1995@users.noreply.github.com>
2018-06-05 22:14:58 +0800

    修复bug：刷新后无论哪个路由，navBar始终是newSongs高亮

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,27 @@
       Head,
       'player-panel': playerPanel,
       'detail-player': detailPlayer,
+    },
+    created(){
+      //判断当前路由
+      let routerPath = this.$router.history.current.path;
+      this.setNav(routerPath)
+    },
+    methods:{
+      setNav (routerPath) {
+        if (routerPath.includes('/rank')) {
+          this.$store.commit('setHeadNav','head-nav2');
+        }
+        else if (routerPath.includes('/plist')) {
+          this.$store.commit('setHeadNav','head-nav3');
+        }
+        else if (routerPath.includes('/singer')) {
+          this.$store.commit('setHeadNav','head-nav4');
+        }
+        else{
+          this.$store.commit('setHeadNav','head-nav1');
+        }
+      }
     }
   }
   </script>
